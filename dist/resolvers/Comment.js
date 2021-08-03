fb599dc0027d039faf4c9c998831f2d59775ab2d
Pankaj <meghanipankaj5@gmail.com>
2021-08-02 23:55:53 -0400

    dist removed

--- dist/resolvers/Comment.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    author: (parent, _args, context) => (context.prisma.comment.findFirst({ where: { id: parent.id } }).author()),
-    ticket: (parent, _args, context) => (context.prisma.comment.findFirst({ where: { id: parent.id } }).ticket()),
-};
-//# sourceMappingURL=Comment.js.map
--- dist/resolvers/Project.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    author: (parent, _args, context) => (context.prisma.project.findFirst({ where: { id: parent.id } }).author()),
-    associatedUsers: (parent, _args, context) => (context.prisma.project.findFirst({ where: { id: parent.id } }).associatedUsers()),
-    tickets: (parent, _args, context) => (context.prisma.project.findFirst({ where: { id: parent.id } }).tickets())
-};
-//# sourceMappingURL=Project.js.map
